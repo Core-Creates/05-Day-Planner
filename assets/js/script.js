e3ce14b5d65310834b57cd49f116ab7d2da8c7c1
Corrina Alcoser <corrinaalcoser@gmail.com>
2023-06-28 15:46:53 -0500

    added json file and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import PropTypes from 'prop-types'
+// import Header from '../../common/Header/'
+// import Masonry from '../../common/Masonry/'
+// import { cardAction } from '../../store/actions/Cards'
+// import Arrow_Down from '../../assets/img/arrow-down.svg'
+
+// const name = document.querySelector(".name")
+
+function changeDay() {}
