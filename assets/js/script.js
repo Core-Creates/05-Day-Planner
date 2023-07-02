0f70eab950fed5ffa93e3e84b1a44f37fece5a35
Corrina Alcoser <corrinaalcoser@gmail.com>
2023-07-01 22:38:37 -0500

    added javaScript to generate card for To Do items

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,53 @@
 // import { cardAction } from '../../store/actions/Cards'
 // import Arrow_Down from '../../assets/img/arrow-down.svg'
 
-// const name = document.querySelector(".name")
+function changeDay() {
 
-function changeDay() {}
+    var el_monday = document.getElementById("monday");
+    var el_tuesday = document.getElementById("tuesday");
+    var el_wednesday = document.getElementById("wednesday");
+    var el_thursday = document.getElementById("thursday");
+    var el_friday = document.getElementById("friday");
+    var el_saturday = document.getElementById("saturday");
+    var el_sunday = document.getElementById("sunday");
+
+    let container = document.querySelector(".container");
+
+    el_monday.addEventListener("click", () => {
+        // alert('monday schedule');
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+        // var newCard = "To Do: " + el_monday.getAttribute("title") + "<";
+    });
+    el_tuesday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+    el_wednesday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+    el_thursday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+    el_friday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+    el_saturday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+    el_sunday.addEventListener("click", () => {
+        let divForSchedule = document.createElement("div");
+        divForSchedule.innerHTML = "<div class=\"card\">"+ "To Do: " +"</div>";
+        container.appendChild(divForSchedule);
+    });
+}
